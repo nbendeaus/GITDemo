ddaac338b4d9289829e51730cd9af3c40d82b45f
Nagaraju <nbende@gmail.com>
2019-05-10 15:57:53 +0530

    d;lfkgsdfl;k

--- WebApplication55/Global.asax.cs
+++ WebApplication55/Global.asax.cs
@@ -11,7 +11,7 @@ namespace WebApplication55
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
-        {
+        {// made some changes
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
